1339df530d55ec0272e1d878795f247f944d2fae
Aditya Tiwari <pubgmlord25@gmail.com>
2023-09-16 12:22:11 +0530

    ci/dd implementation

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM node:alpine
 COPY . /app
 WORKDIR /app
+EXPOSE 80
 CMD node app.js
--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello Docker");
+const http = require('http');
+const port = process.env.PORT || 80;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Docker\n');
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
